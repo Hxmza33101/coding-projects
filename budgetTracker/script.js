c9b9e77a0a1c1125f192a97f8c62a5e85345df10
Hxmza33101 <hkmughal2007@gmail.com>
2026-04-16 19:35:49 +1000

    Started logic for the budget tracker in JS file

---
+++ budgetTracker/script.js
@@ -0,0 +1,12 @@
+//? Checklist for the budget tracker logic
+//* - Add state object that will hold all variables together
+//* - Add balance variable
+//* - Add income and expense objects
+// - Create function that calculates balance based on the income and expenses
+
+let state = {
+    balance: 0,
+    goal: 0,
+    income: [],
+    spending: []
+}
